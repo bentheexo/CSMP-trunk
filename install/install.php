2e36fdeeb31c1c241b03fc7f38249f545ef6b8e5
BenTheExo <bhenderson904@gmail.com>
2012-03-04 10:07:22 -0500

    Added Upgrade SQL
    Signed-off-by: BenTheExo <bhenderson904@gmail.com>

--- install/install.php
+++ install/install.php
@@ -100,7 +100,7 @@ if (isset($POST_install)) {
 	$query = "DROP TABLE IF EXISTS `vote_point`;";
 	$result = mysqli_query($rag_db, $query);
 
-	$query = "CREATE TABLE `vote_point` (`account_id` int(11) NOT NULL default '0',`point` int(11) NOT NULL default '0',`last_vote1` int(11) NOT NULL default '0',`last_vote2` int(11) NOT NULL default '0',`last_vote3` int(11) NOT NULL default '0',`date` text NOT NULL,PRIMARY KEY  `account_id`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
+	$query = "CREATE TABLE `vote_point` (`account_id` int(11) NOT NULL default '0',`point` int(11) NOT NULL default '0',`last_vote1` int(11) NOT NULL default '0',`last_vote2` int(11) NOT NULL default '0',`last_vote3` int(11) NOT NULL default '0',`date` text NOT NULL,PRIMARY KEY  (`account_id`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
 	$result = mysqli_query($rag_db, $query);
 	
 	$query = "DROP TABLE IF EXSISTS `dc_comments`;";
